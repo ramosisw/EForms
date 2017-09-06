20916ed3264202f442ade2c2d5a8177b1e19ec21
ramosisw <ramos.isw@gmail.com>
2017-09-06 17:20:43 -0500

    F

---
+++ example_forms/ViewModels/AboutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace example_forms {
+	public class AboutViewModel : BaseViewModel {
+		public AboutViewModel() {
+			Title = "About";
+
+			OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+		}
+
+		public ICommand OpenWebCommand { get; }
+	}
+}
